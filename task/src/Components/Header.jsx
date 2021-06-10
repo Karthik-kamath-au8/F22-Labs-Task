9c5289c9951457dc2f460dab3086b5e3c5b4fbb2
karthik <kamathkarthik13@gmail.com>
2021-06-10 12:30:20 +0530

    header

---
+++ task/src/Components/Header.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Omega from '../Assests/omega-copy.png';
+import Book  from '../Assests/b-1.png';
+import Path from '../Assests/path_2.png';
+import Oval from '../Assests/oval.png';
+
+import './Header.css'
+
+
+export default class Header extends Component {
+
+    render() {
+      return (
+        <>  
+        <nav className='nav'>
+        <img className='image' src={Omega} alt="" />
+        <div className= 'list'>
+         <p className='active'>Demos</p>
+          <p>Pages</p>
+          <p>Support</p>
+        </div>
+        <button>Get this Book</button>
+        </nav>
+        <div className="bookheader">
+            <img className='bookimage' src={Book} alt=''/>
+            <div>
+            <div></div>    
+            <p className='Solve-your-daily-lif'>Solve your daily life problems in 1 minute.</p>
+            <div className="ovpath">
+              <div className='oval'> 
+             <img className='path' src={Path}  alt=''/> 
+             </div>   
+            <p>12 Chapter with detail illustrations</p>
+            </div>
+            <div className="ovpath">
+              <div className='oval'> 
+             <img className='path' src={Path} alt=''/> 
+             </div>   
+             <p>Learn from the expert with 24 years experience</p>
+            </div>
+            <div className="ovpath">
+              <div className='oval'> 
+             <img className='path' src={Path}  alt=''/> 
+             </div>   
+             <p>Audio version is included with the purchase</p>
+            </div>
+            <div className='price'>
+            <button className='btn'>Get this book - Starts from $29</button>
+            </div>
+            <p>Interested in a free chapter?<a href="">Get it now</a></p>
+            </div>
+
+        </div>
+        <div>
+            <div className="ovalComment">
+            <img className='Ovalimage' src={Oval} alt=""/>
+            <div>
+            <p className="quote">“OMG! I cannot believe that I have got a brand new landing page<br/> after getting Omega. It was super easy to edit and publish.”</p>
+            <p className='taylor'>Jonathan Taylor</p>
+            </div>
+            </div> 
+        </div>
+        </>
+      )
+    }
+  
+  }
